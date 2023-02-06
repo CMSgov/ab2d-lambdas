c05182680bdad19beda9b08e52748841917e001d
Keane McGough <keane.mcgough@icf.com>
2023-02-06 15:33:29 -0600

    update readmes, rename terraform files to match standards, clean up start script

--- coverage-counts/README.md
+++ coverage-counts/README.md
@@ -1,12 +1,20 @@
-Receives coverage counts from other services through SNS messages. 
+## Description 
+Receives coverage counts from other services through SNS messages.
 
-## Build
+## Goal
+Occasionally the data AB2D pulls from BFD is incorrect. HPMS also maintains current bene counts.
+By comparing the counts from BFD and HPMS it's possible to detect these data issues. 
+AB2D's counts before updating from BFD are also included to check if AB2S has already been affected by incorrect data. 
 
-AWS lambdas need to be zipped. The follow command will build the code and zip the resulting jar.
-```
-gradle buildZip
-```
+## Overall design
 
-## Deploy
+New beneficiaries are pulled from BFD once a week for the past 3 months. AB2D workers currently handle this task.
+Before initiating the pull, workers will send current counts for all contracts that will be updated. 
+After each contract/year/month task is completed the counts will be sent to coverage-counts lambda.
 
-https://jenkins.ab2d.cms.gov/job/AB2D-Ops/job/Terraform/job/Deploy%20Lambda/
+HPMS counts will flow from the contract service. A seperat lambda will be scheduled to request the counts from contract service then forward them to this lambda.  
+
+
+## Build/Deploy/etc
+
+Please see the [README.md](../README.md) in the root directory of this project.
--- start.sh
+++ start.sh
@@ -1,24 +1,4 @@
 #!/bin/bash
-timeoutPortable() {
-
-    expect
-    mac=$?
-
-    if [ $mac -eq 127 ] ; then
-      echo "looks like you're using linux"
-      timeout $1 bash -c -- "$2"
-    else
-      #atempt at making this work on mac but don't have one to test on
-      time=$1
-      command="/bin/sh -c \"$2\""
-      echo "looks like you're using a mac"
-      expect -c "set echo \"-noecho\"; set timeout $time; spawn -noecho $command; expect timeout { exit 1 } eof { exit 0 }"
-      if [ $? = 1 ] ; then
-          echo "Timeout after ${time} seconds"
-      fi
-    fi
-
-}
 
 export TF_LOG="TRACE"
 
@@ -26,9 +6,9 @@ if [ "$1" = "clean" ]; then
   ./gradlew clean
 fi
 
-# run docker-compose in detach mode to prevent flooding the console. If you have problems check a specific container's logs with docker logs --follow <container>
 docker-compose up db localstack -d &
-timeoutPortable 30 "while docker container inspect -f '{{.State.Running}}' postgres localstack | sed -e '1h;2,\$H;\$!d;g' -e '/^true\ntrue/p;q' >> /dev/null ; do sleep  5 && echo "checking if docker containers are up"; done"
+command="while docker container inspect -f '{{.State.Running}}' postgres localstack | sed -e '1h;2,\$H;\$!d;g' -e '/^true\ntrue/p;q' >> /dev/null ; do echo \"checking if docker containers are up\" >> test.txt && sleep  5; done"
+perl -e 'alarm shift; exec $command' "30";
 
 ./gradlew clean buildZip
 
@@ -36,4 +16,4 @@ if [ ! -f ./terraform/terraform.tfstate ]; then
   docker-compose -f ./docker-compose.yml run --rm terraform init -upgrade
 fi
 
-docker-compose -f ./docker-compose.yml run --rm terraform apply --auto-approve -var branch_name="${name}"
+docker-compose -f ./docker-compose.yml run --rm terraform apply --auto-approve
