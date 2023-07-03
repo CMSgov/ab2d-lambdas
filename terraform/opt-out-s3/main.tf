19316fc8aa04c82c2d2510f42e47f870d02e9fdd
Sean Fern <seanfern@navapbc.com>
2023-07-03 10:30:16 -0400

    Add region to bucket name

--- terraform/opt-out-s3/main.tf
+++ terraform/opt-out-s3/main.tf
@@ -1,13 +1,15 @@
 # Temporary S3 bucket for opt-out
 
 data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
 
 locals {
   account_id = data.aws_caller_identity.current.account_id
+  region     = data.aws_region.current.name
 }
 
 resource "aws_s3_bucket" "opt_out" {
-  bucket = "${local.account_id}-ab2d-opt-out-temp"
+  bucket = "ab2d-opt-out-temp-${local.account_id}-${local.region}"
   tags = {
     business    = "oeda"
     application = "ab2d"
@@ -24,8 +26,8 @@ resource "aws_s3_bucket_versioning" "opt_out" {
 
 resource "aws_s3_bucket_logging" "opt_out" {
   bucket        = aws_s3_bucket.opt_out.id
-  target_bucket = "cms-cloud-${local.account_id}-us-east-1"
-  target_prefix = "AWSLogs/${local.account_id}/s3/${local.account_id}-ab2d-opt-out-temp-access-logs/"
+  target_bucket = "cms-cloud-${local.account_id}-${local.region}"
+  target_prefix = "AWSLogs/${local.account_id}/s3/${aws_s3_bucket.opt_out.id}-access-logs/"
 }
 
 resource "aws_s3_bucket_policy" "opt_out" {
@@ -39,8 +41,8 @@ resource "aws_s3_bucket_policy" "opt_out" {
         "Principal" : "*",
         "Action" : "s3:*",
         "Resource" : [
-          "arn:aws:s3:::${local.account_id}-ab2d-opt-out-temp",
-          "arn:aws:s3:::${local.account_id}-ab2d-opt-out-temp/*"
+          "${aws_s3_bucket.opt_out.arn}",
+          "${aws_s3_bucket.opt_out.arn}/*"
         ],
         "Condition" : {
           "Bool" : {
