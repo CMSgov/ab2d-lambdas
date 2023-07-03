479e4ab7db284469826b3b5e219b2173e280cf8a
smirnovaae <annasmirnova@navapbc.com>
2023-07-03 10:46:10 -0700

    fix test

--- optout/src/test/java/gov/cms/ab2d/optout/OptOutConsumerTest.java
+++ optout/src/test/java/gov/cms/ab2d/optout/OptOutConsumerTest.java
@@ -2,9 +2,12 @@ package gov.cms.ab2d.optout;
 
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.concurrent.BlockingQueue;
@@ -12,13 +15,16 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.LinkedBlockingQueue;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class OptOutConsumerTest {
 
     @Test
-    public void optOutConsumerRunTest() throws InterruptedException {
+    public void optOutConsumerRunTest() throws InterruptedException, SQLException {
         Connection dbConnection = Mockito.mock(Connection.class);
+        when(dbConnection.prepareStatement(any())).thenReturn(Mockito.mock(PreparedStatement.class));
         CountDownLatch latch = Mockito.mock(CountDownLatch.class);
         LambdaLogger logger = Mockito.mock(LambdaLogger.class);
         BlockingQueue<OptOutMessage> queue = new LinkedBlockingQueue<>();
