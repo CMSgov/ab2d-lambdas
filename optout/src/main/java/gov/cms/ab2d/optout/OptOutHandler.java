391e50ce98744e92750cb346661d0b4292f03da5
Anna Smirnova <132938234+smirnovaae@users.noreply.github.com>
2023-07-14 10:49:38 -0700

    AB2D-5568 (#60)

--- optout/src/main/java/gov/cms/ab2d/optout/OptOutHandler.java
+++ optout/src/main/java/gov/cms/ab2d/optout/OptOutHandler.java
@@ -1,13 +1,15 @@
 package gov.cms.ab2d.optout;
 
+import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -21,6 +23,8 @@ public class OptOutHandler implements RequestStreamHandler {
         LambdaLogger logger = context.getLogger();
         logger.log("OptOut Lambda is started");
 
+      //  downloadFileFromS3(logger);
+
         //ToDo: important! add capacity for backpressure
         BlockingQueue<OptOutMessage> queue = new LinkedBlockingQueue<>();
 
@@ -31,13 +35,21 @@ public class OptOutHandler implements RequestStreamHandler {
         //ToDo: replace on S3
         ClassLoader classLoader = getClass().getClassLoader();
         try {
-            File file = new File(Objects.requireNonNull(classLoader.getResource("optOutDummy.txt")).getFile());
+            File file = new File(Objects.requireNonNull(classLoader.getResource(Constants.fileName)).getFile());
+            //for s3
+          //  File file = new File(Constants.filePath);
             Connection dbConnection = DatabaseUtil.getConnection();
 
             executorService.execute(new OptOutProducer(queue, file, latch, logger));
             executorService.execute(new OptOutConsumer(queue, dbConnection, latch, logger));
 
             latch.await();
+
+//            if (file.delete()) {
+//                System.out.println("Deleted the file: " + Constants.fileName);
+//            } else {
+//                System.out.println("Failed to delete the file.");
+//            }
         } catch (NullPointerException | SQLException | InterruptedException ex) {
             logger.log(ex.getMessage());
             outputStream.write(ex.getMessage().getBytes(StandardCharsets.UTF_8));
@@ -46,8 +58,34 @@ public class OptOutHandler implements RequestStreamHandler {
             shutdownAndAwaitTermination(executorService, logger);
             outputStream.write("OptOut Lambda Completed".getBytes(StandardCharsets.UTF_8));
         }
-
     }
+    //Commented to avoid unnecessary work with temporary credentials
+/*
+    private void downloadFileFromS3(LambdaLogger logger) {
+        logger.log("Downloading " + Constants.fileName + " from S3 bucket " + Constants.s3BucketName);
+        final AmazonS3 s3 = AmazonS3ClientBuilder.standard()
+                .withRegion(Constants.s3Region).build();
+        try {
+            S3Object o = s3.getObject(Constants.s3BucketName, Constants.fileName);
+            S3ObjectInputStream s3is = o.getObjectContent();
+            FileOutputStream fos = new FileOutputStream(Constants.filePath);
+            byte[] read_buf = new byte[1024];
+            int read_len = 0;
+            while ((read_len = s3is.read(read_buf)) > 0) {
+                fos.write(read_buf, 0, read_len);
+            }
+            s3is.close();
+            fos.close();
+        } catch (AmazonServiceException ex) {
+            logger.log(ex.getErrorMessage());
+            throw new OptOutException(ex);
+        } catch (IOException ex) {
+            logger.log(ex.getMessage());
+            throw new OptOutException(ex);
+        }
+    }
+
+ */
 
     private void shutdownAndAwaitTermination(ExecutorService executorService, LambdaLogger logger) {
         logger.log("Call ThreadPoll shutdown");
