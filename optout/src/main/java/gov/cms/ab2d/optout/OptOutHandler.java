cb1ebdefe656a467b6fac62e163f2abd73f2b7d7
Anna Smirnova <132938234+smirnovaae@users.noreply.github.com>
2023-10-25 10:52:38 -0700

    AB2D-5797 Copy Opt-out file from BFD S3 to AB2D hosted S3 for backup(inbound) (#65)

--- optout/src/main/java/gov/cms/ab2d/optout/OptOutHandler.java
+++ optout/src/main/java/gov/cms/ab2d/optout/OptOutHandler.java
@@ -17,37 +17,37 @@ public class OptOutHandler implements RequestStreamHandler {
         LambdaLogger logger = context.getLogger();
         logger.log("OptOut Lambda is started");
 
-      //  downloadFileFromS3(logger);
-
-        //ToDo: important! add capacity for backpressure
-        BlockingQueue<OptOutMessage> queue = new LinkedBlockingQueue<>();
-
-        int threadCount = 2; //producer and consumer
+        int threadCount = 2;
         ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
         CountDownLatch latch = new CountDownLatch(threadCount);
 
-        //ToDo: replace on S3
         try {
-            InputStream fileInputStream = getClass().getResourceAsStream("/" + OptOutUtils.FILE_NAME);
-/*
-            for s3
-            File file = new File(OptOutUtils.FILE_PATH);
-
- */
-            Connection dbConnection = DatabaseUtil.getConnection();
-
-            executorService.execute(new OptOutProducer(queue, fileInputStream, latch, logger));
-            executorService.execute(new OptOutConsumer(queue, dbConnection, latch, logger));
+            executorService.execute(new OptOutS3(true, latch, logger)); // download file from BFD S3 locally
+            executorService.execute(new OptOutS3(false, latch, logger)); // copy files from BFD S3 to AB2D S3
 
             latch.await();
-/*
+
+            //ToDo: important! add capacity for backpressure
+            BlockingQueue<OptOutMessage> queue = new LinkedBlockingQueue<>();
+
+
+            File file = new File(OptOutUtils.FILE_PATH);
+            FileInputStream fileInputStream = new FileInputStream(file);
+
+            Connection dbConnection = DatabaseUtil.getConnection();
+
+            latch = new CountDownLatch(threadCount);
+
+            executorService.execute(new OptOutProducer(queue, fileInputStream, latch, logger)); //parse file
+            executorService.execute(new OptOutConsumer(queue, dbConnection, latch, logger)); //update database
+
+            latch.await();
+
             if (file.delete()) {
                 System.out.println("Deleted the file: " + OptOutUtils.FILE_NAME);
             } else {
                 System.out.println("Failed to delete the file.");
             }
-
- */
         } catch (NullPointerException | InterruptedException ex) {
             logger.log(ex.getMessage());
             outputStream.write(ex.getMessage().getBytes(StandardCharsets.UTF_8));
@@ -57,32 +57,7 @@ public class OptOutHandler implements RequestStreamHandler {
             outputStream.write("OptOut Lambda Completed".getBytes(StandardCharsets.UTF_8));
         }
     }
-    //Commented to avoid unnecessary work with temporary credentials
-/*
-    private void downloadFileFromS3(LambdaLogger logger) {
-        logger.log("Downloading " + OptOutUtils.FILE_NAME + " from S3 bucket " + OptOutUtils.S3_BUCKET_NAME);
-        final AmazonS3 s3 = AmazonS3ClientBuilder.standard()
-                .withRegion(OptOutUtils.S3_REGION).build();
-        try {
-            S3Object o = s3.getObject(OptOutUtils.S3_BUCKET_NAME, OptOutUtils.FILE_NAME);
-            S3ObjectInputStream s3is = o.getObjectContent();
-            FileOutputStream fos = new FileOutputStream(OptOutUtils.FILE_PATH);
-            byte[] read_buf = new byte[1024];
-            int read_len = 0;
-            while ((read_len = s3is.read(read_buf)) > 0) {
-                fos.write(read_buf, 0, read_len);
-            }
-            s3is.close();
-            fos.close();
-        } catch (AmazonServiceException ex) {
-            logger.log(ex.getErrorMessage());
-            throw new OptOutException(ex);
-        } catch (IOException ex) {
-            logger.log(ex.getMessage());
-            throw new OptOutException(ex);
-        }
-    }
-*/
+
 
     private void shutdownAndAwaitTermination(ExecutorService executorService, LambdaLogger logger) {
         logger.log("Call ThreadPoll shutdown");
