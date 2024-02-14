2c37786c47dce7d569d3b03e70974e9d3abce844
Anna Smirnova <132938234+smirnovaae@users.noreply.github.com>
2024-02-14 13:14:18 -0800

    Ab2d-5913/Splunk alerts attribution data share (#80)

--- attribution-data-file-share/src/main/java/gov/cms/ab2d/attributionDataShare/AttributionDataShareException.java
+++ attribution-data-file-share/src/main/java/gov/cms/ab2d/attributionDataShare/AttributionDataShareException.java
@@ -1,7 +1,7 @@
 package gov.cms.ab2d.attributionDataShare;
 
 public class AttributionDataShareException extends RuntimeException {
-    public AttributionDataShareException(Exception ex) {
-        super(ex);
+    public AttributionDataShareException(String message, Exception ex) {
+        super(message, ex);
     }
 }
