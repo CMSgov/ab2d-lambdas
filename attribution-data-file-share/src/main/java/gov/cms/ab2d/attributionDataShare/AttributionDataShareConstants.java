9c616c5d2bc7ea578d251c1abd5e7a8063a1ded0
Anna Smirnova <132938234+smirnovaae@users.noreply.github.com>
2024-03-13 14:37:48 -0700

    Ab2d-6007/update reguest file (#92)

--- attribution-data-file-share/src/main/java/gov/cms/ab2d/attributionDataShare/AttributionDataShareConstants.java
+++ attribution-data-file-share/src/main/java/gov/cms/ab2d/attributionDataShare/AttributionDataShareConstants.java
@@ -12,14 +12,13 @@ public class AttributionDataShareConstants {
     public static final String TEST_ENDPOINT = "http://127.0.0.1:8001";
     public static final Region S3_REGION = Region.US_EAST_1;
     public static final String FILE_PATH = "/tmp/";
-    public static final String REQ_FILE_NAME = "P.AB2D.NGD.REQ.";
+    public static final String REQ_FILE_NAME = "P#EFT.ON.AB2D.NGD.REQ.";
     public static final String REQ_FILE_NAME_PATTERN = "'D'yyMMdd.'T'hhmmsss";
     public static final String FIRST_LINE = "HDR_BENEDATAREQ_";
     public static final String LAST_LINE = "TLR_BENEDATAREQ_";
     public static final String SELECT_STATEMENT = "SELECT * FROM public.current_mbi";
     public static final int CURRENT_MBI_LENGTH = 11;
     public static final String EFFECTIVE_DATE_PATTERN = "yyyyMMdd";
-    public static final int EFFECTIVE_DATE_LENGTH = 8;
     public static final String BUCKET_NAME_PROP = "S3_UPLOAD_BUCKET";
     public static final String UPLOAD_PATH_PROP = "S3_UPLOAD_PATH";
 
