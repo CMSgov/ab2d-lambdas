ae0967ada4d5cda7e10ac610986b9e7a21ba0923
Sean Fern <seanfern@navapbc.com>
2024-04-04 10:21:22 -0400

    Check confirmation file in integration test

---
+++ optout/src/test/resources/check-conf-file.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+# Echo to stderr
+err() {
+  echo 2>&1 $1
+}
+
+conf_file=$1
+today=$(date +%Y%m%d)
+expected=$(mktemp)
+
+# Use head from gnu coreutils to remove trailing newline
+head -c-1 <<EOF > "$expected"
+HDR_BENECONFIRM${today}
+1S00E00JG37${today}NAccepted  00
+7SP1D00AA00${today}NAccepted  00
+2SY1D00AA00${today}YAccepted  00
+7SF9C00AA00${today}NAccepted  00
+6SF9C00AA00${today}NAccepted  00
+DUMMY000006${today}YAccepted  00
+DUMMY000007${today}NAccepted  00
+TRL_BENECONFIRM${today}0000000007
+EOF
+
+err "Testing confirmation file"
+diff -u "$conf_file" "$expected"
