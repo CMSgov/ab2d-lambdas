30c5aa9ed3b2d96d2e031b5901e1b5f6452f3090
Sean Fern <seanfern@navapbc.com>
2024-05-13 13:30:45 -0400

    Add script for checking latest and fix expected

--- optout/src/test/resources/check-conf-file.sh
+++ optout/src/test/resources/check-conf-file.sh
@@ -11,13 +11,10 @@ conf_file=$1
 today=$(date +%Y%m%d)
 expected=$(mktemp)
 
-err "Expected file content:"
-err
-
 # Use head from gnu coreutils to remove trailing newline
-head -c-1 <<EOF | >&2 tee "$expected"
+head -c-1 <<EOF > "$expected"
 HDR_BENECONFIRM${today}
-1S00E00JG37${today}NAccepted  00
+1S00E00JG37${today}YAccepted  00
 7SP1D00AA00${today}NAccepted  00
 2SY1D00AA00${today}YAccepted  00
 7SF9C00AA00${today}NAccepted  00
@@ -27,13 +24,9 @@ DUMMY000007${today}NAccepted  00
 TRL_BENECONFIRM${today}0000000007
 EOF
 
-# Print some newlines to make logs easier to read
-err
-err
-
 err "Testing confirmation file..."
 
-# Hide output for now to avoid revealing sensitive info
+# Hide output to avoid revealing sensitive info
 if ! diff -u "$conf_file" "$expected" > /dev/null; then
   err "Confirmation file differs from expected"
   exit 1
