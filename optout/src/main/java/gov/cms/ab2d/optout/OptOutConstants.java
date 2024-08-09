99824766f7406e92d59df2d44c104595709e78b8
Sadibhatla <sravanti.adibhatla@gmail.com>
2024-08-08 17:42:28 -0700

    Changed opt out flag to share_data

--- optout/src/main/java/gov/cms/ab2d/optout/OptOutConstants.java
+++ optout/src/main/java/gov/cms/ab2d/optout/OptOutConstants.java
@@ -25,9 +25,9 @@ public class OptOutConstants {
             "SET share_data = ?, effective_date = current_date\n" +
             "WHERE mbi = ?";
     public static final String COUNT_STATEMENT = "SELECT \n"+
-        "COUNT(CASE WHEN opt_out_flag = 'true' THEN 1 END) AS optin, \n"+
-        "COUNT(CASE WHEN opt_out_flag = 'false' THEN 1 END) AS optout \n"+
-        "FROM current_mbi WHERE opt_out_flag IS NOT NULL";
+        "COUNT(CASE WHEN share_data = 'true' THEN 1 END) AS optin, \n"+
+        "COUNT(CASE WHEN share_data = 'false' THEN 1 END) AS optout \n"+
+        "FROM current_mbi WHERE share_data IS NOT NULL";
 
     private OptOutConstants() {}
 }
