dd99165563732ebf688e59501de7f27e6bd24881
smirnovaae <annasmirnova@navapbc.com>
2024-10-15 13:17:54 -0700

    Spring boot upgrade

--- lambda-test-utils/src/main/java/gov/cms/ab2d/testutils/AB2DLocalstackContainer.java
+++ lambda-test-utils/src/main/java/gov/cms/ab2d/testutils/AB2DLocalstackContainer.java
@@ -1,14 +1,11 @@
 package gov.cms.ab2d.testutils;
 
-import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.localstack.LocalStackContainer;
 import org.testcontainers.utility.DockerImageName;
 
-import java.util.Collections;
-
 public class AB2DLocalstackContainer extends LocalStackContainer {
 
-    private static final DockerImageName IMAGE_VERSION = DockerImageName.parse("localstack/localstack:1.4.0");
+    private static final DockerImageName IMAGE_VERSION = DockerImageName.parse("localstack/localstack:3.5.0");
 
     public AB2DLocalstackContainer() {
         super(IMAGE_VERSION);
